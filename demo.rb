76ab9391f54f79b2a162480774860cad20dc1202
Andres Riofrio <riofrios@gmail.com>
2012-01-12 22:46:43 -0800

    Use Unix line endings---goodness\!

--- demo.rb
+++ demo.rb
@@ -1,12 +1,12 @@
 require 'waz-queues'
 
-account_name = "raceariofrio"
-access_key = "GDyrT8XE/AaVZuX1XJ08eIHoOCU0pEGeJR/hts5EIvI4xawd7FR9SRVnmtM6qk0y77E6jlONgn4GC/Wm29abIg=="
-
-WAZ::Storage::Base.establish_connection! :account_name => account_name, :access_key => access_key
+WAZ::Storage::Base.establish_connection! \
+	:account_name => ENV['AZURE_STORAGE_ACCOUNT_NAME'],
+	:access_key => ENV['AZURE_STORAGE_ACCESS_KEY']
 
 # Queue.ensure is syntactic sugar for (1) does queue exist? (2) if not, create queue
-puts "making sure queue exists" and queue = WAZ::Queues::Queue.ensure('my-demo-queue')
+puts "making sure queue exists"
+queue = WAZ::Queues::Queue.ensure('my-demo-queue')
 puts "querying size of queue: " + queue.size.to_s
 
 puts "putting message into queue" and queue.enqueue! "hey this is my message"
